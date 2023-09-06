41bcf0b76842b9499b945a466280505f6668c2df
supersonicspeedjj <115861964+supersonicspeedjj@users.noreply.github.com>
2023-09-06 15:06:26 +0530

    Final Changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Newscomp from "./components/Newscomp";
@@ -6,30 +6,39 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import LoadingBar from 'react-top-loading-bar'
 
+function App() {
+  const [progress, setProgress] = useState(0);
+  const [pagem, setPage] = useState("technology");
 
-export default function App() {
-  const [pagem, setPage] = useState("technology"); // Using state hook to manage 'page' state
+  useEffect(() => {
+    document.body.style.background = "#1F1B24";
+  }, []);
 
   function pgchange(page) {
     if (page === "technology") {
-      setPage("technology"); // Update 'page' state when the function is called
-    } else if(page==="sports") {
+      setPage("technology");
+    } else if (page === "sports") {
       setPage("sports");
+    } else {
+      setPage("politics");
     }
-    else
-    setPage("politics"); // Update 'page' state when the function is called
   }
-
-  document.body.style.background = "#1F1B24";
-
+  function changepro(progress){
+    setProgress(progress);
+  }
   return (
     <>
-   
-      <Navbar  pgchange={pgchange}></Navbar>
-      
-      
-      <Newscomp page={pagem}></Newscomp>
+      <Navbar pgchange={pgchange}></Navbar>
+      <LoadingBar
+        color='#f11946'
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
+      <Newscomp page={pagem} changepro={changepro}></Newscomp>
     </>
   );
 }
+
+export default App;
--- src/components/Newscomp.js
+++ src/components/Newscomp.js
@@ -16,18 +16,20 @@ function Newscomp(props) {
   const [truth,settruth]=useState(false);
   const [publish,setpub]=useState([]);
   const [auth,setau]=useState([]);
-  const [results,setresult]=useState([]);
+  // const [results,setresult]=useState([]);
   const [vari,setvari]=useState(1);
   useEffect(() => {
+    // eslint-disable-next-line 
     async function fetchData() {
       try {
         const url1 = `https://newsdata.io/api/1/news?apikey=pub_2841359190a0d184cf49552e79fa72b27cfd6&language=en&category=${props.page}&image=1`;
         settruth(true);
+        props.changepro(10);
         const data = await fetch(url1);
         const processed_data = await data.json();
         console.log(processed_data.totalResults	)
       
-        
+        props.changepro(30);
         setpoger(processed_data.nextPage);
         setvari(1);
         const titles = processed_data.results.map((article) => article.title);
@@ -36,7 +38,8 @@ function Newscomp(props) {
         const urls = processed_data.results.map((itera) => itera.link);
         const publishes=processed_data.results.map((itera)=>itera.pubDate);
         const author=processed_data.results.map((itera)=>itera.source_id);
-        setresult(processed_data.totalResults	);
+        props.changepro(60);
+        // setresult(processed_data.totalResults	);
         setArticleTitles(titles);
         setDesc(descriptions);
         setImage(images);
@@ -44,6 +47,8 @@ function Newscomp(props) {
         settruth(false);
         setpub(publishes);
         setau(author);
+        props.changepro(90);
+        props.changepro(100);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -55,14 +60,15 @@ function Newscomp(props) {
 
   async function fetcher(pageno) {
     try {
-      
+      // eslint-disable-next-line 
       const url1 = `https://newsdata.io/api/1/news?apikey=pub_2841359190a0d184cf49552e79fa72b27cfd6&category=${props.page}&language=en&image=1&page=${pageno}`;
       console.log(props.page);
       settruth(true);
+      props.changepro(10);
       const data = await fetch(url1);
       const processed_data = await data.json();
       console.log(processed_data)
-     
+      props.changepro(30);
       setpoger(processed_data.nextPage);
       const titles = processed_data.results.map((article) => article.title);
       const descriptions = processed_data.results.map((desi) => desi.description);
@@ -70,7 +76,8 @@ function Newscomp(props) {
       const urls = processed_data.results.map((itera) => itera.link);
       const publishes=processed_data.results.map((itera)=>itera.pubDate);
       const author=processed_data.results.map((itera)=>itera.source_id);
-      setresult(processed_data.totalResults	);
+      props.changepro(60);
+      // setresult(processed_data.totalResults	);
       setArticleTitles(articleTitles.concat(titles));
       setDesc(desc.concat(descriptions));
       setImage(image.concat(images));
@@ -79,6 +86,8 @@ function Newscomp(props) {
       setpub(publish.concat(publishes));
       setau(auth.concat(author));
       setvari(vari+1);
+      props.changepro(90);
+      props.changepro(100);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -141,12 +150,12 @@ function Newscomp(props) {
   </div>
       <Grid
         container
-        spacing={{ xs: 1, md: 3 }}
+        spacing={{ xs: 2, md: 3 }}
         columns={{ xs: 4, sm: 8, md: 12 }}
-        style={{ marginLeft: matches ? "40px" : "-5px", marginRight: "0px" }}
+        style={{ marginLeft: matches ? "25px" : "-5px", marginRight: "0px" }}
       >
         {Array.from(Array(matches?9*vari:10*vari)).map((_, index) => (
-          <Grid item xs={2} sm={4} md={4} key={index}>
+          <Grid item xs={matches?2:4} sm={4} md={4} key={index}  > 
         <Newsitem
            
           title={
