614fc6b54dd3280de64d789391c180bf8fdbc253
Atishay Jain <115861964+supersonicspeedjj@users.noreply.github.com>
2023-09-26 14:05:13 +0530

    Update Newscomp.js

--- src/components/Newscomp.js
+++ src/components/Newscomp.js
@@ -27,6 +27,7 @@ function Newscomp(props) {
     
     async function fetchData() {
       try {
+        clevertap.getAllInboxMessages() 
         clevertap.event.push("Product Viewed");
         const url1 = `https://newsdata.io/api/1/news?apikey=pub_2841359190a0d184cf49552e79fa72b27cfd6&language=en&category=${props.page}&image=1`;
         settruth(true);
