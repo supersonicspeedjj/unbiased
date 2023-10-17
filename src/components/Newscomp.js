a1b56221949779c47cd644125f9bf2a8a47e5465
Atishay Jain <115861964+supersonicspeedjj@users.noreply.github.com>
2023-10-17 13:00:45 +0530

    changes

--- src/components/Newscomp.js
+++ src/components/Newscomp.js
@@ -120,7 +120,7 @@ function Newscomp(props) {
       >
         UnBiased Headlines
       </h1>
-      <div id="container">
+      <div id="container" style={{height:"600px",width:"100%"}}>
 	
 </div>
   <div style={{display:"flex",justifyContent:"center",alignContent:"center",marginTop:"20px",marginBottom:"20px"}}>{truth&&<CircularProgress style={{color:"white"}}/>}
